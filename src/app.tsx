d58204985aa8e7a0cf34f207b316e88acd09c2af
dzianis kavaliou <ds.kavaliou@gmail.com>
2025-06-06 08:47:40 +0200

    feat: finished task 2

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,21 @@
-import { PricingList } from "./features/product-list";
-
 export function App() {
   return (
-    <section>
-      <div className="max-w-7xl mx-auto">
-        <PricingList />
-      </div>
-    </section>
+    <nav className="nav">
+      <input type="checkbox" id="menuToggle" className="menu-toggle" />
+      <label htmlFor="menuToggle" className="hamburger">
+        ☰ Menu
+      </label>
+      <ul className="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
